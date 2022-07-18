8aac2a44f2c519c2a98ae64797d3776b2273feb1
Wojciech Repiński <wrepinski@gmail.com>
2022-07-18 15:18:24 +0200

    fix perfmeter

--- local/share/check_mk/web/plugins/perfometer/jetpower.py
+++ local/share/check_mk/web/plugins/perfometer/jetpower.py
@@ -5,8 +5,9 @@ from cmk.gui.plugins.metrics import perfometer_info
 
 perfometer_info.append({
 #                    "type": "logarithmic",
+                    "segments": ["system_current_load"],
                     "type": "linear",
-                    "metric": "system_current_load",
+#                    "metric": "system_current_load",
 #                    "half_value": 1.0,
 #                    "exponent": 2.0,
 		     "total": 2.0,
